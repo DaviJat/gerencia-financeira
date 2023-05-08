dbbda4ea72380ef78a579910b17d033dadf1d962
DaviJat <91758001+DaviJat@users.noreply.github.com>
2023-05-08 12:00:59 -0300

    Chamando validação de login no front

--- src/components/CardLogin.tsx
+++ src/components/CardLogin.tsx
@@ -1,15 +1,30 @@
+import { useState } from 'react';
+import * as router from '../pages/api/router';
 import styles from '../styles/cardLogin.module.css';
 
 export default function CardLogin() {
+  const [usuario] = useState({
+    email: '',
+    senha: '',
+    service: '',
+  });
+
+  function loginUsuario(e: any) {
+    usuario.service = e.target.name;
+    router.apiPost(usuario, 'usuario');
+  }
+
   return (
     <div className={styles.card}>
       <h2>Login</h2>
-      <label htmlFor="">Digite seu email:</label>
-      <input type="text" />
-      <label htmlFor="">Digite sua senha:</label>
-      <input type="text" />
+      <label htmlFor="email">Digite seu email:</label>
+      <input id="email" name="email" type="text" onChange={(e) => (usuario.email = e.target.value)} />
+      <label htmlFor="password">Digite sua senha:</label>
+      <input id="password" name="password" type="password" onChange={(e) => (usuario.senha = e.target.value)} />
       <a href="">Esqueceu sua senha?</a>
-      <button>Entrar</button>
+      <button name="loginUsuario" onClick={loginUsuario}>
+        Entrar
+      </button>
     </div>
   );
 }
