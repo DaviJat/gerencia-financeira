b90fc11a5bb5f77ab2d1d75783d11ce3bfb7d966
DaviJat <91758001+DaviJat@users.noreply.github.com>
2023-06-05 09:59:38 -0300

    Adicionando mudança de card

--- src/components/CardLogin.tsx
+++ src/components/CardLogin.tsx
@@ -9,6 +9,8 @@ export default function CardLogin() {
     service: '',
   });
 
+  const [form, setForm] = useState('login');
+
   function loginUsuario(e: any) {
     usuario.service = e.target.name;
     router.apiPost(usuario, 'usuario');
@@ -16,21 +18,43 @@ export default function CardLogin() {
 
   return (
     <div className={styles.card}>
-      <h2>Login</h2>
-      <label htmlFor="email">Digite seu email:</label>
-      <input id="email" name="email" type="email" onChange={(e) => (usuario.email = e.target.value)} />
-      <label htmlFor="password">Digite sua senha:</label>
-      <input id="password" name="password" type="password" onChange={(e) => (usuario.senha = e.target.value)} />
-      <a className={styles.forgotPassword} href="">
-        Esqueceu sua senha?
-      </a>
-      <button name="loginUsuario" onClick={loginUsuario}>
-        Entrar
-      </button>
-      <div className={styles.registerContainer}>
-        <p>Primeiro acesso?</p>
-        <a href="">Cadastre-se</a>
-      </div>
+      {form == 'login' ? (
+        <>
+          <h2>Login</h2>
+          <label htmlFor="email">Digite seu email:</label>
+          <input id="email" name="email" type="email" onChange={(e) => (usuario.email = e.target.value)} />
+          <label htmlFor="password">Digite sua senha:</label>
+          <input id="password" name="password" type="password" onChange={(e) => (usuario.senha = e.target.value)} />
+          <a className={styles.forgotPassword} href="">
+            Esqueceu sua senha?
+          </a>
+          <button name="loginUsuario" onClick={loginUsuario}>
+            Entrar
+          </button>
+          <div className={styles.registerContainer}>
+            <p>Primeiro acesso?</p>
+            <span onClick={() => setForm('cadastro')}>Cadastre-se</span>
+          </div>
+        </>
+      ) : (
+        <>
+          <h2>Cadastro</h2>
+          <label htmlFor="email">Digite seu email:</label>
+          <input id="email" name="email" type="email" onChange={(e) => (usuario.email = e.target.value)} />
+          <label htmlFor="password">Digite sua senha:</label>
+          <input id="password" name="password" type="password" onChange={(e) => (usuario.senha = e.target.value)} />
+          <a className={styles.forgotPassword} href="">
+            Esqueceu sua senha?
+          </a>
+          <button name="loginUsuario" onClick={loginUsuario}>
+            Entrar
+          </button>
+          <div className={styles.registerContainer}>
+            <p>Primeiro acesso?</p>
+            <span onClick={() => setForm('login')}>Cadastre-se</span>
+          </div>
+        </>
+      )}
     </div>
   );
 }
